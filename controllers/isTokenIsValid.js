093008af07f8941ea80e9f65db7e01be2fce3e9d
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-02-07 14:56:18 -0300

    Requisito 3

--- controllers/isTokenIsValid.js
+++ controllers/isTokenIsValid.js
@@ -5,12 +5,15 @@ const secret = process.env.JWT_SECRET;
 
 module.exports = async (token) => {
   if (token === undefined) return { status: 401, response: { message: 'Token not found' } };
-  
-  const decoded = jwt.verify(token, secret);
+  try {
+    const decoded = jwt.verify(token, secret);
 
-  const user = await Users.findOne({ where: { email: decoded.data } });
-  console.log(user);
-  if (!user) return { status: 401, response: { message: 'Expired or invalid token' } };
+    const user = await Users.findOne({ where: { email: decoded.data } });
   
+    if (!user) return { status: 401, response: { message: 'Expired or invalid token' } };
+  } catch (error) {
+    console.log(error.message);
+    return { status: 401, response: { message: 'Expired or invalid token' } };
+  }
   return true;
 };
