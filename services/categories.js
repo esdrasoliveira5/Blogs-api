643861e6730807ab21f6b685dc9d6388bdc06847
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-02-07 16:41:04 -0300

    lint Correcao

--- services/categories.js
+++ services/categories.js
@@ -2,7 +2,6 @@ const isTokenIsValid = require('../helpers/isTokenIsValid');
 const { Categories } = require('../models');
 
 const createCategories = async (token, name) => {
-
   const validToken = await isTokenIsValid(token);
 
   if (!name) return { status: 400, response: { message: '"name" is required' } };
